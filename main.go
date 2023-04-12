64e58a522709870e4edfec29258b65c786e7925e
Cyprien Borée <boreec@tuta.io>
2023-04-13 00:46:40 +0900

    feat: url redirection snippet

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+var NewUrl string = "https://github.com"
 
 func main() {
-	fmt.Println("Hello World!")
+	http.Handle("/", http.RedirectHandler(NewUrl, http.StatusMovedPermanently))
+	log.Fatal(http.ListenAndServe(":9000", nil))
 }
