c0825716328a21611e7e923c7e967b645c0fec58
Cyprien Borée <boreec@tuta.io>
2023-04-19 23:30:26 +0900

    chore: rename urlList into urlMap

--- handler/url_shortener_handler.go
+++ handler/url_shortener_handler.go
@@ -6,11 +6,11 @@ import (
 )
 
 type URLShortenerHandler struct {
-	urlList map[string]string
+	urlMap map[string]string
 }
 
 func NewURLShortenerHandler() *URLShortenerHandler {
-	return &URLShortenerHandler{urlList: make(map[string]string)}
+	return &URLShortenerHandler{urlMap: make(map[string]string)}
 }
 
 func (url_shortener *URLShortenerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -22,10 +22,10 @@ func (url_shortener *URLShortenerHandler) ServeHTTP(w http.ResponseWriter, r *ht
 	url := r.Form.Get("long_link")
 
 	// check if url is already contained in urlList
-	if url_shortener.urlList[url] != "" {
-		w.Write([]byte(fmt.Sprintf("link %s is already shortened as %s", url, url_shortener.urlList[url])))
+	if url_shortener.urlMap[url] != "" {
+		w.Write([]byte(fmt.Sprintf("link %s is already shortened as %s", url, url_shortener.urlMap[url])))
 	} else {
-		url_shortener.urlList[url] = url_shortener.hash(url)
+		url_shortener.urlMap[url] = url_shortener.hash(url)
 	}
 
 	w.Write([]byte(fmt.Sprintf("to do: shorten %v", url_shortener)))
