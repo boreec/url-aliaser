f1f5de18fe150d6348b04a8a991fed1a9dc7d981
Cyprien Borée <boreec@tuta.io>
2023-04-20 23:08:30 +0900

    test: test that hash returns an error when an empty string is provided

--- handler/url_shortener_handler_test.go
+++ handler/url_shortener_handler_test.go
@@ -2,6 +2,10 @@ package handler
 
 import "testing"
 
-func TestHash(t *testing.T) {
-	t.Fatal("to do \n")
+func TestHashWithEmptyString(t *testing.T) {
+	shortenerHandler := NewURLShortenerHandler()
+
+	if _, err := shortenerHandler.hash("", 10); err == nil {
+		t.Fatal("hash function should return error when empty string\n")
+	}
 }
