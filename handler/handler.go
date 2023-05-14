f5f07b6ca312c9445c0f17ad3d89c0f6f43cdd70
Cyprien Borée <boreec@tuta.io>
2023-05-15 00:38:04 +0900

    fix: simple handling in handler/handler.go [skip actions]

--- handler/handler.go
+++ handler/handler.go
@@ -1,5 +1,10 @@
 package handler
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 // expected payload in the request
 type PayloadRequest struct {
 	Url    string `json:"url"`              // required
@@ -9,4 +14,23 @@ type PayloadRequest struct {
 // payload returned
 type PayloadResponse struct {
 	Url string `json:"url"`
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		var payloadRequest PayloadRequest
+		err := json.NewDecoder(r.Body).Decode(&payloadRequest)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Return the JSON payload as the response
+		var payloadResponse PayloadResponse
+		payloadResponse.Url = "to do"
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(payloadResponse)
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
 }
