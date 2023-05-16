38bfc1a83b88984f740142423ca74af84b601031
Cyprien Borée <boreec@tuta.io>
2023-05-17 00:03:24 +0900

    test: add unit test for HandleRequest when GET method is provided and error is expected

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,1 +1,22 @@
 package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandleRequestFailsOnGETRequest(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	// Create a ResponseRecorder to record the handler response
+	rr := httptest.NewRecorder()
+
+	// Call the handler function with the mock request and response recorder
+	HandleRequest(rr, req)
+
+	// Check if the handler returned an error
+	assert.Equal(t, http.StatusMethodNotAllowed, rr.Result().StatusCode)
+}
